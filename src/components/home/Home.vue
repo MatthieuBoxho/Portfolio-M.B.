61b6bbb915e731ebb9a6de7316161fa9e27efd12
Boxho <matthieu@gmail.com>
2018-01-09 16:51:58 +0100

    Created project architecture with seperated files template/style/script

---
+++ src/components/home/Home.vue
@@ -0,0 +1,17 @@
+<template src="./home.html">
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Welcome to Yours Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped src="./home.scss" lang="scss">
+</style>
